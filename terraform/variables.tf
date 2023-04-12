c4733c5a42a513c3eb647d96a032545f9494954c
pawelpesz <81652898+pawelpesz@users.noreply.github.com>
2023-04-12 14:27:02 +0100

    Flux version and other tweaks (#2)
    * Added Flux version variable.
    * Renamed the GitHub token variable.
    * K8s config file and context will be supplied in TF variables, instead of envvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,16 +2,31 @@ variable "github_owner" {
   type = string
 }
 
-variable "repository_name" {
-  type = string
-}
-
-variable "flux_token" {
+variable "github_token" {
   type      = string
   sensitive = true
+}
+
+variable "repository_name" {
+  type = string
 }
 
 variable "target_sync_path" {
   type    = string
   default = "clusters/staging"
+}
+
+variable "flux_version" {
+  type    = string
+  default = "v2.0.0-rc.1"
+}
+
+variable "kube_config_path" {
+  type    = string
+  default = "~/.kube/config"
+}
+
+variable "kube_config_context" {
+  type    = string
+  default = null
 }
