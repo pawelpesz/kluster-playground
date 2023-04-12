89b4938277e91b3e4d9ee0804911d84757446e05
Pawel Pesz <pawel.pesz@gmail.com>
2023-04-12 12:42:02 +0100

    Update Flux Terraform provider to 1.0.0 * Flux provider 0.25.3 -> 1.0.0-rc.1 * Replaced the deprecated approach based on data sources with the one using a single resource. * Added Flux version variable. * Renamed the GitHub token variable. * K8s config file and context will be supplied in TF variables, instead of envvars.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,24 +3,21 @@ terraform {
   required_providers {
     flux = {
       source  = "fluxcd/flux"
-      version = "~> 0.25"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.19"
+      version = "1.0.0-rc.1"
     }
   }
 }
 
 provider "flux" {
-  # config_path from KUBE_CONFIG_PATH
-}
-
-provider "kubernetes" {
-  # config_path from KUBE_CONFIG_PATH
-  # config_context from KUBE_CTX
+  kubernetes = {
+    config_path    = var.kube_config_path
+    config_context = var.kube_config_context
+  }
+  git = {
+    url = "https://github.com/${var.github_owner}/${var.repository_name}.git"
+    http = {
+      username = var.github_owner
+      password = var.github_token
+    }
+  }
 }
