44e5499c0b1d4144cf5614156d6d4b5d0bca5f08
Pawel Pesz <pawel.pesz@gmail.com>
2023-04-27 14:40:03 +0100

    feat: Enabled Helm near OOM detection * This is to prevent Helm releases getting stuck in pending-* state.

--- terraform/flux-install.tf
+++ terraform/flux-install.tf
@@ -1,6 +1,7 @@
 # All-in-one install
 resource "flux_bootstrap_git" "flux" {
-  version        = var.flux_version
-  path           = var.target_sync_path
-  network_policy = false
+  version                = var.flux_version
+  path                   = var.target_sync_path
+  network_policy         = false
+  kustomization_override = file("${path.module}/kustomization.yaml")
 }
